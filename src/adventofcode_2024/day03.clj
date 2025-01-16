50d7c6b488d1e4ad26090d2d83e11a16aef2b44f
Magnus Ekstrand <magnusekstrand.net>
2025-01-16 16:28:09 +0100

    Add solution for day 3 part 2 (FAIL)

--- src/adventofcode_2024/day03.clj
+++ src/adventofcode_2024/day03.clj
@@ -6,7 +6,7 @@
   (re-seq #"mul\((\d{1,3}),(\d{1,3})\)" (apply str input)))
 
 ;; calculate mul -> x * y
-;; input is [mul(2,4) 2 4]
+;; input should [mul(2,4) 2 4]
 (defn- calc-muls
   [input]
   (if input
@@ -14,7 +14,8 @@
           y (Integer/parseInt (last input))]
       (* x y))))
 
-
+;; input -> xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+;; result -> 161
 (defn part-1
   "Day 03 Part 1"
   [input]
@@ -23,8 +24,50 @@
        (map calc-muls)
        (reduce +)))
 
-;(defn part-2
-;  "Day 03 Part 2"
-;  [input]
-;  (->> input
-;       u/to-lines))
+;; input ->
+;; xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+(defn- find-muls-2
+  [input]
+  (re-seq #"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)" (apply str input)))
+
+;; input ->
+;; ([mul(2,4) 2 4 nil] [don't() nil nil nil] [mul(5,5) 5 5 nil] [mul(11,8) 11 8 nil] [do() nil nil do()] [mul(8,5) 8 5 nil])
+(defn- remove-nils
+  [input]
+  (clojure.walk/postwalk
+    #(if (coll? %)
+       (into (empty %) (remove nil? %))
+       %) input))
+
+;; input ->
+;; ([mul(8,5) 8 5] [do() do()] [mul(11,8) 11 8] [mul(5,5) 5 5] [don't()] [mul(2,4) 2 4])
+(defn- filter-muls
+  [input]
+  (loop [[x & xs] input
+         use true
+         values ()]
+    (if (not (nil? x))
+      (cond
+        (= "do()" (first x)) (recur xs true values)
+        (= "don't()" (first x)) (recur xs false values)
+        :else (if use
+                (recur xs use (cons x values))
+                (recur xs use values)))
+      (identity values)
+      )
+    ))
+
+;; input -> xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+;; result -> 48
+;; NOTE: on training data (above) the calculation is correct,
+;;       but at the real test data the calculated value is incorrect (too low)
+(defn part-2
+  "Day 03 Part 2"
+  [input]
+  (->> input
+       (find-muls-2)
+       (remove-nils)
+       (filter-muls)
+       (map calc-muls)
+       (reduce +)))
+
